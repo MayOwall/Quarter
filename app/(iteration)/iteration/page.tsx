a2d067e5b4639e8bf7ea2579885f9e5fa863ce06
MayOwall <pixartive@gmail.com>
2024-02-08 20:13:34 +0900

    Feat: Quarter 페이지 및 Iteration 페이지 간의 라우팅 구현
    - Quarter 페이지 및 Iteration 페이지 생성
    - Quarter 페이지 및 Iteration 페이지 간의 라우팅 구현

---
+++ app/(iteration)/iteration/page.tsx
@@ -0,0 +1,10 @@
+import { GlobalHeader } from "components";
+
+export default function IterationPage() {
+  return (
+    <>
+      <GlobalHeader type="iteration" />
+      <main>{`<Main 😆/>`}</main>
+    </>
+  );
+}
---
+++ app/(quarter)/page.tsx
@@ -0,0 +1,10 @@
+import { GlobalHeader } from "components";
+
+export default function QuarterPage() {
+  return (
+    <>
+      <GlobalHeader />
+      <main>{`<Main 😆/>`}</main>
+    </>
+  );
+}
